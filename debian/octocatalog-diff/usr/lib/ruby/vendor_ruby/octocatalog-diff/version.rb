eab04d4ec33397dc27f1114efb527ed53d76a27c
root <root@ocf.berkeley.edu>
2023-05-17 12:42:32 -0700

    fix: remove auto-generated files

--- debian/octocatalog-diff/usr/lib/ruby/vendor_ruby/octocatalog-diff/version.rb
+++
@@ -1,5 +0,0 @@
-module OctocatalogDiff
-  class Version
-    VERSION = "1.5.4-2ocf1".freeze
-  end
-end
--- debian/octocatalog-diff/usr/share/octocatalog-diff/scripts/env/env.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# This script echoes back the environment. This is used for spec testing
-# and possible debugging.
-
-env
